b839283a019f33aae5e02f51787267712ef0275e
rjcalifornia <rodolfohernan20@gmail.com>
2019-12-03 14:47:48 +0000

    Initial release

---
+++ src/Controller/SextansApiController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use App\Entity\Contacts;
+
+
+class SextansApiController extends AbstractController
+{
+    /**
+     * @Route("/", name="sextans_api")
+     */
+    public function index()
+    {
+        /*
+        return $this->render('sextans_api/index.html.twig', [
+            'controller_name' => 'SextansApiController',
+        ]);*/
+        
+        return $this->render('home/index.html.twig');
+    }
+    
+    /**
+     * Returns a JSON with the contacts stored in the database
+     *
+     * @Route("/api/v1/show-users/", name="show_users_api")  
+     * 
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function ShowAllUsersAction(Request $request)
+    {
+        header("Access-Control-Allow-Origin: *");
+        $em = $this->getDoctrine()->getManager();
+         $query = $em->createQuery('SELECT m 
+                  FROM App:Contacts m
+                  ORDER BY m.id ASC');
+         $contacts = $query->getResult();
+         
+         $baseurl = $request->getSchemeAndHttpHost();
+         
+         $responseArray = array();
+        foreach($contacts as $obj){
+            $responseArray[] = array(
+                "id" => $obj->getId(),
+                "name" => $obj->getFullname(),
+                "username" => $obj->getUsername(),
+                "email" => $obj->getEmail(),
+                "phone" => $obj->getPhone(),
+                 "filename" => $baseurl .'/sextan_api/public/uploads/brochures/'. $obj->getFilename(),
+                    
+            );
+            
+                     
+        }
+        return new JsonResponse($responseArray);
+    }
+    
+    /**
+     * Stores a contact in the database
+     *
+     * @Route("/api/v1/save-user/", name="save_user_api")  
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function saveSingleUserAction(Request $request)
+    {
+        header("Access-Control-Allow-Origin: *");
+        $em = $this->getDoctrine()->getManager();
+        
+      //  $data = json_decode($request->getContent(), true);
+        $data = $request;
+        
+        
+        if($data != null){
+            
+        $get_file = $request->files->get('file');
+        $temp_name = pathinfo($get_file->getClientOriginalName(), PATHINFO_FILENAME);
+        $file_name = $temp_name.'-'.uniqid().'.'.$get_file->guessExtension();
+        
+        try 
+                {
+                   
+                    $get_file->move(
+                        $this->getParameter('file_storage'),
+                        $file_name
+                    );
+                }
+                
+                
+                catch (FileException $e) {
+                    
+                }
+
+        $get_fullname =$data->get('fullname');
+        $get_username =$data->get('username');
+        $get_email =$data->get('email');
+        $get_phone =$data->get('phone');
+        
+        $contact = new Contacts();
+        
+        $contact->setFullname($get_fullname);
+        $contact->setUsername($get_username);
+        $contact->setEmail($get_email);
+        $contact->setPhone($get_phone);
+        $contact->setFilename($file_name);
+        $em->persist($contact);
+        $em->flush();
+        
+        $responseArray[] = array(
+                "id" => $contact->getFullname(),
+                "message" => 'Contact stored successfully'
+            );
+        
+            }
+            else
+            {
+                $responseArray[] = ["message" => "No data received", "error-code" =>  "400" ];
+            }
+            
+        
+         return new JsonResponse($responseArray);
+
+    }
+}
---
+++ src/Entity/Contacts.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContactsRepository")
+ */
+class Contacts
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=700, nullable=true)
+     */
+    private $fullname;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=700)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $phone;
+    
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $filename;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFullname(): ?string
+    {
+        return $this->fullname;
+    }
+
+    public function setFullname(?string $fullname): self
+    {
+        $this->fullname = $fullname;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(?string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+    
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(?string $filename): self
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+}
